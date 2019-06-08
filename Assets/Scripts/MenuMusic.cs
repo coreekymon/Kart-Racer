8eb5a5eb34657086fdf5d2cb48f35ea97ef9e597
coreekymon <50118585+coreekymon@users.noreply.github.com>
2019-06-08 02:25:08 -0700

    Menu Screen much improved

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusic : MonoBehaviour
+{
+    public string SceneName;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if(objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene CurrentScene = SceneManager.GetActiveScene();
+        SceneName = CurrentScene.name;
+        if(SceneName == "FieldTrack" || SceneName == "MoonTrack" || SceneName == "SnowyMountainTrack" || SceneName == "DesertTrack:" || SceneName == "CityTrack")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
