89c85bd4d478ce189a98ddbbc9e88a2fdd1658e4
coreekymon <50118585+coreekymon@users.noreply.github.com>
2019-06-09 14:10:14 -0700

    working on save data

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public int fieldminutes = 2;
+    public int fieldseconds = 0;
+    public int fieldmseconds = 0;
+    public int mountainminutes = 3;
+    public int mountainseconds = 0;
+    public int mountainmseconds = 0;
+    public int desertminutes = 7;
+    public int desertseconds = 0;
+    public int desertmseconds = 0;
+    public int moonminutes = 7;
+    public int moonseconds = 0;
+    public int moonmseconds = 0;
+    public int cityminutes = 10;
+    public int cityseconds = 0;
+    public int citymseconds = 0;
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("savedata");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
